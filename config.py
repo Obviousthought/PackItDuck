c2c778e1ac2b6f57a243c27121ad7e23e1903c6d
Obviousthought <shawnpohlson@Shawns-MacBook-Air.local>
2013-12-05 12:14:01 -0800

    modifications

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 # Config file, put all your keys and passwords and whatnot in here
 DB_URI = os.environ.get("DATABASE_URL", "sqlite:///packinglist.db")
+# DB_URI = os.environ.get("DATABASE_URL", "postgresql:///packinglist.db")
 SECRET_KEY = "this should be a secret"
 
 # db = DB_URI(app)
